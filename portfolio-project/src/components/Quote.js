a829da051bfd8a02585458115bab3b7e283962a1
sueKK <125456919+sueKK@users.noreply.github.com>
2025-01-10 17:35:31 +0300

    add quote section

---
+++ portfolio-project/src/components/Quote.js
@@ -0,0 +1,15 @@
+// Quote.js
+import React from "react";
+
+const Quote = () => {
+  return (
+    <div className="p-8 bg-[#f4e7ce] text-center">
+      <p className="text-2xl font-serif">
+        \"Coding with purpose, innovating with passion, and building a future
+        where creativity meets technology.\"
+      </p>
+    </div>
+  );
+};
+
+export default Quote;
